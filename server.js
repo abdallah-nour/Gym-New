3e5fa54f3f0334ee1418bff4055740304c4c76cf
Brayan Rodriguez Montealegre <brayan7890123@gmail.com>
2022-07-04 17:10:04 -0500

    Deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 connectDB();
 
-// Init middleware
+// Init middleware development
 
 app.use(express.json({ extended: false }));
 
@@ -22,10 +22,16 @@ app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.development === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send("API running");
+    });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
